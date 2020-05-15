29a5288b10a82a695280b5feb0f4de04dabbf8ee
Tahlil1 <bsse0803@iit.du.ac.bd>
2020-05-15 15:23:15 +0600

    added body parser

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const port = 3000
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 require('./Routes/routes.index.js')(app);
+
 
 app.listen(port);
