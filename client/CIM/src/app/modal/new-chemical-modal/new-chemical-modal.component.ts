4cea5510852457e3ff0f91b0e8c27ff99b1f5ecc
Tahlil <bsse0803@iit.du.ac.bd>
2022-01-02 19:07:54 +0600

    Bind add new chemical data

--- client/CIM/src/app/modal/new-chemical-modal/new-chemical-modal.component.ts
+++ client/CIM/src/app/modal/new-chemical-modal/new-chemical-modal.component.ts
@@ -8,11 +8,49 @@ import { ModalController } from '@ionic/angular';
 })
 export class NewChemicalModalComponent implements OnInit {
 
+  name: string;
+  casNumber: string;
+  sds: string;
+  unitType: string;
+  quantity: number;
+  place: string;
+
   constructor(public modalController: ModalController) { }
 
   ngOnInit() {}
 
   async closeModal() {
+    await this.modalController.dismiss('Cancel');
+  }
+  // event:any,value:any){
+  //   console.log(event.detail.data)
+  onNameChange(event:any){
+    this.name = event.detail.value;  
+  }
+
+  onCASNumberChange(event:any){
+    this.casNumber = event.detail.value;  
+  }
+
+  onSDSChange(event:any){
+    this.sds = event.detail.value;  
+  }
+
+  onUnitTypeChange(event:any){
+    this.unitType = event.detail.value;  
+  }
+
+  onQuantityChange(event:any){
+    this.quantity = event.detail.value;  
+  }
+
+  onPlaceChange(event:any){
+    this.place = event.detail.value;  
+  }
+
+  async addNewChemical() {
+    console.log("Adding new chemical.");
+    console.log(this.name, this.casNumber, this.sds, this.unitType, this.quantity, this.place);
     await this.modalController.dismiss('Cancel');
   }
 
